e04c68bcd90bf1a3f4f96013fb006e6c1c033011
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-09-15 17:33:02 -0300

    Room - create new video

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { roomRepository } from '../repositories/RoomRepository';
+import { videoRepository } from '../repositories/VideoRepository';
 
 export class RoomController {
     async create(req: Request, res: Response) {
@@ -17,6 +18,36 @@ export class RoomController {
             await roomRepository.save(newRoom);
 
             return res.status(201).json(newRoom);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ message: 'internal server error' });
+        }
+    }
+
+    async createVideo(req: Request, res: Response) {
+        const { title, url } = req.body;
+        const { id } = req.params;
+
+        if (!title || !url || !id) {
+            return res.status(400).json({
+                message: `expected title: string, url: string, id: number`,
+            });
+        }
+
+        try {
+            const room = await roomRepository.findOneBy({ id: Number(id) });
+
+            if (!room) {
+                return res
+                    .status(404)
+                    .json({ message: `room with id ${id} not found` });
+            }
+
+            const newVideo = videoRepository.create({ title, url, room });
+
+            await videoRepository.save(newVideo);
+
+            return res.status(201).json(newVideo);
         } catch (error) {
             console.log(error);
             return res.status(500).json({ message: 'internal server error' });
--- src/routes.ts
+++ src/routes.ts
@@ -7,5 +7,6 @@ const routes = Router();
 routes.post('/subjects', new SubjectController().create);
 
 routes.post('/rooms', new RoomController().create);
+routes.post('/rooms/:id/video', new RoomController().createVideo);
 
 export default routes;
