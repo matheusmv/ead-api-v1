89c764857390e0e9ac983df987e76a655770337c
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-09-15 18:12:35 -0300

    Room - add subject to room and list all rooms

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { roomRepository } from '../repositories/RoomRepository';
+import { subjectRepository } from '../repositories/SubjectRepository';
 import { videoRepository } from '../repositories/VideoRepository';
 
 export class RoomController {
@@ -48,6 +49,62 @@ export class RoomController {
             await videoRepository.save(newVideo);
 
             return res.status(201).json(newVideo);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ message: 'internal server error' });
+        }
+    }
+
+    async addSubject(req: Request, res: Response) {
+        const { subject_id } = req.body;
+        const { id } = req.params;
+
+        if (!subject_id || !id) {
+            return res.status(400).json({
+                message: `expected subject_id: number, id: number`,
+            });
+        }
+
+        try {
+            const room = await roomRepository.findOneBy({ id: Number(id) });
+
+            if (!room) {
+                return res
+                    .status(404)
+                    .json({ message: `room with id ${id} not found` });
+            }
+
+            const subject = await subjectRepository.findOneBy({
+                id: Number(subject_id),
+            });
+
+            if (!subject) {
+                return res
+                    .status(404)
+                    .json({ message: `subject with id ${id} not found` });
+            }
+
+            const updatedRoom = {
+                ...room,
+                subjects: [subject],
+            };
+
+            await roomRepository.save(updatedRoom);
+
+            return res.status(204).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ message: 'internal server error' });
+        }
+    }
+
+    async list(req: Request, res: Response) {
+        try {
+            const rooms = await roomRepository.find({
+                relations: { subjects: true },
+            });
+
+            return res.status(200).json(rooms);
         } catch (error) {
             console.log(error);
             return res.status(500).json({ message: 'internal server error' });
--- src/routes.ts
+++ src/routes.ts
@@ -6,7 +6,9 @@ const routes = Router();
 
 routes.post('/subjects', new SubjectController().create);
 
+routes.get('/rooms', new RoomController().list);
 routes.post('/rooms', new RoomController().create);
 routes.post('/rooms/:id/video', new RoomController().createVideo);
+routes.patch('/rooms/:id/subject', new RoomController().addSubject);
 
 export default routes;
