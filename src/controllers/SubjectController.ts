0ea9c8747fde1e9f040598a1fc0595ab0c352528
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-09-15 16:36:36 -0300

    Subject - add controller and repository

---
+++ src/controllers/SubjectController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { subjectRepository } from '../repositories/SubjectRepository';
+
+export class SubjectController {
+    async create(req: Request, res: Response) {
+        const { name } = req.body;
+
+        if (!name) {
+            return res
+                .status(400)
+                .json({ message: 'name must not be empty/null' });
+        }
+
+        try {
+            const newSubject = subjectRepository.create({ name });
+
+            await subjectRepository.save(newSubject);
+
+            return res.status(201).json(newSubject);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ message: 'internal server error' });
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { AppDataSource } from './data-source';
+import routes from './routes';
 
 AppDataSource.initialize().then(() => {
     const App = express();
 
     App.use(express.json());
-
-    App.get('/', (req: Request, res: Response) => {
-        return res.status(200).send('ok');
-    });
+    App.use(routes);
 
     const port = process.env.API_PORT ?? 3001;
 
